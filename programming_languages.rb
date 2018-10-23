434d66b665f18563047d768fdcee3967c31e3544
Cristian  CC <cristi.nord@gmail.com>
2018-10-23 10:30:04 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -35,5 +35,11 @@ languages = {
 def reformat_languages(languages)
   hash = {}
   languages.each do |type, data|
-    data[:ruby]
+    data.each do |name, attributes|
+      hash[name] || = attributes
+      hash[name][:style] = []
+      hash[name][:style] << style
+    end
+  end
+  return hash
 end
